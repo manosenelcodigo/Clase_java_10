f7142fb8d0b55faaa95f8a304271fb1effc1446d
#manosenelcódigo <peligrosamente@gmail.com>
2016-06-23 23:06:30 -0300

    Create Ejecutar.java

---
+++ Ejecutar.java
@@ -0,0 +1,14 @@
+
+package com.manosenelcodigo.controller;
+
+import com.manosenelcodigo.graficos.Marco;
+
+
+public class Ejecutar 
+{
+    public static void main(String[] args) 
+    {
+        Marco marco=new Marco();
+        marco.getVentana();
+    }
+}
